2666488c5b9c93713899233a76913a4ced95992e
preye omusuku <omusukup@yahoo.com>
2023-03-19 11:39:47 +0100

    commit

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -111,7 +111,7 @@ button {
     right: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(50, 40, 40, 0.25);
+    background-color: rgba(50, 40, 40, 0);
     box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
     backdrop-filter: blur(5rem);
     justify-content: center;
